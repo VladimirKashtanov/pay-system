0c8cd8f082d08579bb1f8c3df19e216965a6607a
Vladimir <vladimir.kharitonov.04@mail.ru>
2026-02-13 19:54:59 +0300

    Completed USER_CREATE operation (in UserService class)

--- src/main/java/org/voldymar/Main.java
+++ src/main/java/org/voldymar/Main.java
@@ -2,14 +2,11 @@ package org.voldymar;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.voldymar.account.Account;
-import org.voldymar.account.AccountService;
-import org.voldymar.account.AccountStatus;
-import org.voldymar.shared.service.UserWithAccountsService;
 import org.voldymar.user.User;
 import org.voldymar.user.UserService;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,23 +14,11 @@ public class Main {
                 new AnnotationConfigApplicationContext("org.voldymar");
 
         var userService = context.getBean(UserService.class);
-        var accountService = context.getBean(AccountService.class);
-        var service = context.getBean(UserWithAccountsService.class);
-
         userService.createUser(new User(
-                0L,
-                "test@test.domen",
-                Set.of(0L)
+                null,
+                "test@domen.test",
+                new HashSet<Long>()
         ));
-
-        accountService.createAccount(new Account(
-                0L,
-                100.0,
-                AccountStatus.ACTIVE,
-                0L
-        ));
-
-        System.out.println("user email: " + service.getAllUsers().getFirst().email() +
-                "\naccount id: " + service.getAllUsers().getFirst().accounts().stream().toList().getFirst().id());
+        System.out.println("User created successfully: " + userService.getById(0L).get().email());
     }
 }
--- src/main/java/org/voldymar/user/UserService.java
+++ src/main/java/org/voldymar/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,11 +21,22 @@ public class UserService {
 
 
     public User createUser(User user) {
+        var existing = userRepository.findById(user.id());
+        if (existing.isPresent()) {
+            throw new IllegalArgumentException("User with e-mail=\"" +
+                    existing.get().email() + "\" already exists");
+        }
+
         return userRepository.save(user);
     }
 
 
     public List<User> getAll() {
         return userRepository.findAll();
+    }
+
+
+    public Optional<User> getById(Long id) {
+        return userRepository.findById(id);
     }
 }
